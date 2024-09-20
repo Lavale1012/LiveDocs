241b8dbfb7b0ed77f07a1c0a81c1dcb3d6b3e3a6
Lavale1012 <lavale889@gmail.com>
2024-09-20 12:32:20 -0400

    first commit: this is the start or my livedocs project at this moment the project is being set up

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+const Home = () => {
+  return (
+    <div>
+      <Button>Button</Button>
+    </div>
+  );
+};
+
+export default Home;
---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import "./globals.css";
+import { Inter as FontSans } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+export const metadata: Metadata = {
+  title: "LiveDocs",
+  description: "My App",
+};
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn("min-h-screen  font-sans antialiased", fontSans.variable)}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
